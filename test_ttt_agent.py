488fe6249950ae91165b5ad95ab1290109906558
Sharif Elfouly <selfouly@gmail.com>
2017-12-19 17:27:11 +0100

    added test cases for agent methods

--- test_ttt_agent.py
+++ test_ttt_agent.py
@@ -1,5 +1,6 @@
 import unittest
-from tic_tac_toe_agent import create_empty_board, get_n_of_tokens_on_board, place_token_on_board, get_value_of_game_state, Agent
+from tic_tac_toe_agent import create_empty_board, get_n_of_tokens_on_board,place_token_on_board, get_value_of_game_state, Agent, get_place_of_token_to_get_to_next_state, \
+    subtract_list_from_list, get_empty_places_on_board
 
 
 class TestTicTacToeAgentMethods(unittest.TestCase):
@@ -55,6 +56,86 @@ class TestTicTacToeAgentMethods(unittest.TestCase):
 
         self.assertEqual(agent.get_next_states(1), [[[1, 0, 0, 0, -1, 0, 0, 0, 0], [0]]])
 
+    def test_get_next_greedy_state(self):
+
+        agent = Agent()
+
+        games_states = [[[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]],
+                        [[1, 0, 0, 0, 0, 0, 0, 0, 0], [0.75]],
+                        [[0, 0, 0, 0, 0, 1, 0, 0, 0], [0.1]],
+                        [[1, 0, 0, 0, -1, 0, 0, 0, 0], [0]],
+                        [[1, 0, 0, -1, 0, 0, 1, 0, 0], [0.8]],
+                        [[1, 0, 0, -1, 0, 0, 1, 1, -1], [0.98]],
+                        [[1, -1, 1, -1, 0, 0, 1, 1, -1], [0.001]]]
+
+        self.assertEqual(agent.get_next_greedy_state(games_states), [1, 0, 0, -1, 0, 0, 1, 1, -1])
+
+        games_states = [[[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]],
+                        [[1, 0, 0, 0, 0, 0, 0, 0, 0], [0.75]],
+                        [[0, 0, 0, 0, 0, 1, 0, 0, 0], [0.1]]]
+
+        self.assertEqual(agent.get_next_greedy_state(games_states), [1, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_get_place_of_token_to_get_to_next_state(self):
+
+        board = [0, 0, 0, 0, 0, 1, 0, 0, 0]
+
+        next_game_state = [0, -1, 0, 0, 1, 1, 0, 0, 0]
+
+        self.assertEqual(get_place_of_token_to_get_to_next_state(board, next_game_state, 1), 4)
+
+        board = [1, 0, -1, 0, 0, 1, 0, 0, 0]
+
+        next_game_state = [1, 0, -1, 0, 0, 1, 0, 1, 0]
+
+        self.assertEqual(get_place_of_token_to_get_to_next_state(board, next_game_state, 1), 7)
+
+    def test_subtract_list_from_list(self):
+
+        list1 = [1, 0, 0]
+        list2 = [0, 0, 1]
+
+        self.assertEqual(subtract_list_from_list(list1, list2), [-1, 0, 1])
+
+        list1 = [1, 0, 0, 0]
+        list2 = [0, 0, 1]
+
+        self.assertRaises(Exception, subtract_list_from_list, list1, list2)
+
+        list1 = [1, 0, 0, 1, -1]
+        list2 = [0, 0, 1, 0, -1]
+
+        self.assertEqual(subtract_list_from_list(list1, list2), [-1, 0, 1, -1, 0])
+
+    def test_add_game_state(self):
+
+        agent = Agent()
+
+        game_state = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]]
+
+        agent.add_game_state(game_state)
+
+        self.assertEqual(agent.game_states, [[[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]]])
+
+        agent.add_game_state([[1, -1, 1, -1, 0, 0, 1, 1, -1], [0.001]])
+
+        self.assertEqual(agent.game_states, [[[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]],
+                                             [[1, -1, 1, -1, 0, 0, 1, 1, -1], [0.001]]])
+
+        agent.add_game_state(game_state)
+
+        self.assertEqual(agent.game_states, [[[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]],
+                                             [[1, -1, 1, -1, 0, 0, 1, 1, -1], [0.001]]])
+
+    def test_get_empty_places_on_board(self):
+
+        board = [0, 0, 0, 0, 1, -1, 0, 1, 0]
+
+        self.assertEqual(get_empty_places_on_board(board), [0, 1, 2, 3, 6, 8])
+
+        board = [1, -1, 1, 0, 1, -1, 1, 1, 0]
+
+        self.assertEqual(get_empty_places_on_board(board), [3, 8])
 
 if __name__ == '__main__':
     unittest.main()
