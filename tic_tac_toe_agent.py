78f6c72013bf40840c936614256f595d30fd1fb5
Sharif Elfouly <selfouly@gmail.com>
2017-12-20 16:17:53 +0100

    started the implementation of the play method

--- tic_tac_toe_agent.py
+++ tic_tac_toe_agent.py
@@ -9,6 +9,7 @@
 
 from random import randint
 
+from board_drawer import display_board, output_board
 
 winning_state = [[[1, 1, 1, 0, 0, 0, 0, 0, 0], [1]],
                  [[0, 0, 0, 1, 1, 1, 0, 0, 0], [1]],
@@ -39,8 +40,32 @@ def is_game_won(board, token):
     return False
 
 
+def is_game_drawn(board):
+    if get_n_of_tokens_on_board(board) < 9:
+        return False
+    if get_n_of_tokens_on_board(board) == 9:
+        if not is_game_won(board, 1) and not is_game_won(board, -1):
+            return False
+
+    return True
+
+
 def create_empty_board():
     return [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+def subtract_list_from_list(list1, list2):
+
+    if len(list1) is not len(list2):
+        raise Exception
+
+    new_list = []
+
+    for i in range(len(list1)):
+        new_element = list2[i] - list1[i]
+        new_list.append(new_element)
+
+    return new_list
 
 
 def get_n_of_tokens_on_board(board):
@@ -83,6 +108,27 @@ def get_next_random_play(board):
     return empty_places_on_board[random_number]
 
 
+def get_next_random_state(board, token):
+    next_random_place = get_next_random_play(board)
+    board[next_random_place] = token
+
+    return create_state_from_board(board, 0.5)
+
+
+def create_new_board(board, place, token):
+    new_board = board
+    new_board[place] = token
+    return new_board
+
+
+def create_state_from_board(board, state_value):
+    new_state = [[], []]
+
+    new_state[0] = board
+    new_state[1] = state_value
+    return new_state
+
+
 def get_value_of_game_state(state):
     return state[1][0]
 
@@ -113,7 +159,7 @@ class Agent:
         if game_state not in self.game_states:
             self.game_states.append(game_state)
 
-    def get_next_states(self, tokens_played):
+    def get_next_states(self, tokens_played, board):
 
         next_game_states = []
 
@@ -126,11 +172,11 @@ class Agent:
 
         return next_game_states
 
-    def get_next_greedy_state(self, next_game_states):
+    def get_next_greedy_state(self, tokens_played, board):
 
         next_greedy_game_state = [[0], [0]]
 
-        for game_state in next_game_states:
+        for game_state in self.get_next_states(tokens_played, board):
             if game_state[1] > next_greedy_game_state[1]:
                 next_greedy_game_state = game_state
 
@@ -145,17 +191,46 @@ class Agent:
 
         print('**************************************************')
 
+    def play_turn(self, board):
 
-def subtract_list_from_list(list1, list2):
+        old_board = list(board)
 
-    if len(list1) is not len(list2):
-        raise Exception
+        if len(self.game_states) == 0 or len(self.get_next_states(get_n_of_tokens_on_board(board), board)) == 0:
 
-    new_list = []
+            next_state = get_next_random_state(old_board, 1)
+            self.add_game_state(next_state)
+            next_place = get_place_of_token_to_get_to_next_state(board, next_state[0], 1)
 
-    for i in range(len(list1)):
-        new_element = list2[i] - list1[i]
-        new_list.append(new_element)
+            board[next_place] = 1
 
-    return new_list
+            self.display_game_states()
 
+            return board
+
+        else:
+
+            next_state = self.get_next_greedy_state(get_n_of_tokens_on_board(board), board)
+            self.add_game_state(next_state)
+            next_place = get_place_of_token_to_get_to_next_state(board, next_state[0], 1)
+
+            if next_place is not -1:
+                board[next_place] = 1
+
+            self.display_game_states()
+
+            return board
+
+
+def play():
+
+    board = create_empty_board()
+
+    agent = Agent()
+
+    agent.play_turn(board)
+
+    print(display_board(board))
+
+
+if __name__ == '__main__':
+    play()
