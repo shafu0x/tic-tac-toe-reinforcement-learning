6f0b0cb333d3e7be4ddb93c3e0e8fc759538c48b
Sharif Elfouly <selfouly@gmail.com>
2017-12-21 22:21:29 +0100

    agent file was renamed

--- test_ttt_agent.py
+++ test_ttt_agent.py
@@ -1,6 +1,6 @@
 import unittest
-from tic_tac_toe_agent import create_empty_board, get_n_of_tokens_on_board,place_token_on_board, get_value_of_game_state, Agent, get_place_of_token_to_get_to_next_state, \
-    subtract_list_from_list, get_empty_places_on_board, is_game_won
+from ttt_agent import create_empty_board, get_n_of_tokens_on_board,place_token_on_board, state_value, Agent, place_to_get_to_state, \
+    sub_board_from_board, empty_places_on_board, is_game_won
 
 
 class TestTicTacToeAgentMethods(unittest.TestCase):
@@ -33,9 +33,9 @@ class TestTicTacToeAgentMethods(unittest.TestCase):
 
     def test_get_value_of_game_state(self):
 
-        self.assertEqual(get_value_of_game_state([[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]]), 0.5)
-        self.assertEqual(get_value_of_game_state([[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.85]]), 0.85)
-        self.assertEqual(get_value_of_game_state([[0, 0, 0, 0, 0, 0, 0, 0, 0], [1]]), 1)
+        self.assertEqual(state_value([[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]]), 0.5)
+        self.assertEqual(state_value([[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.85]]), 0.85)
+        self.assertEqual(state_value([[0, 0, 0, 0, 0, 0, 0, 0, 0], [1]]), 1)
 
     def test_get_next_states_for_agent(self):
 
@@ -82,30 +82,30 @@ class TestTicTacToeAgentMethods(unittest.TestCase):
 
         next_game_state = [0, -1, 0, 0, 1, 1, 0, 0, 0]
 
-        self.assertEqual(get_place_of_token_to_get_to_next_state(board, next_game_state, 1), 4)
+        self.assertEqual(place_to_get_to_state(board, next_game_state, 1), 4)
 
         board = [1, 0, -1, 0, 0, 1, 0, 0, 0]
 
         next_game_state = [1, 0, -1, 0, 0, 1, 0, 1, 0]
 
-        self.assertEqual(get_place_of_token_to_get_to_next_state(board, next_game_state, 1), 7)
+        self.assertEqual(place_to_get_to_state(board, next_game_state, 1), 7)
 
     def test_subtract_list_from_list(self):
 
         list1 = [1, 0, 0]
         list2 = [0, 0, 1]
 
-        self.assertEqual(subtract_list_from_list(list1, list2), [-1, 0, 1])
+        self.assertEqual(sub_board_from_board(list1, list2), [-1, 0, 1])
 
         list1 = [1, 0, 0, 0]
         list2 = [0, 0, 1]
 
-        self.assertRaises(Exception, subtract_list_from_list, list1, list2)
+        self.assertRaises(Exception, sub_board_from_board, list1, list2)
 
         list1 = [1, 0, 0, 1, -1]
         list2 = [0, 0, 1, 0, -1]
 
-        self.assertEqual(subtract_list_from_list(list1, list2), [-1, 0, 1, -1, 0])
+        self.assertEqual(sub_board_from_board(list1, list2), [-1, 0, 1, -1, 0])
 
     def test_add_game_state(self):
 
@@ -131,11 +131,11 @@ class TestTicTacToeAgentMethods(unittest.TestCase):
 
         board = [0, 0, 0, 0, 1, -1, 0, 1, 0]
 
-        self.assertEqual(get_empty_places_on_board(board), [0, 1, 2, 3, 6, 8])
+        self.assertEqual(empty_places_on_board(board), [0, 1, 2, 3, 6, 8])
 
         board = [1, -1, 1, 0, 1, -1, 1, 1, 0]
 
-        self.assertEqual(get_empty_places_on_board(board), [3, 8])
+        self.assertEqual(empty_places_on_board(board), [3, 8])
 
     def test_is_game_won(self):
 
@@ -147,6 +147,48 @@ class TestTicTacToeAgentMethods(unittest.TestCase):
 
         self.assertEqual(is_game_won(board, 1), True)
 
+        board = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+
+        self.assertEqual(is_game_won(board, 1), True)
+
+        board = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+
+        self.assertEqual(is_game_won(board, -1), False)
+
+    def test_update_game_state(self):
+
+        agent = Agent()
+        agent.add_game_state([[0, 0, 1], [0.5]])
+
+        agent.update_game_state_in_list([[0, 0, 1], [1]])
+
+        self.assertEqual(agent.game_states[0][1], [1])
+
+        agent.add_game_state([[-1, 0, 1], [0.2]])
+
+        agent.update_game_state_in_list([[-1, 0, 1], [0.75]])
+
+        self.assertEqual(agent.game_states[1][1], [0.75])
+
+    # TODO: REMOVE
+    def test_update_previous_game_state_value(self):
+
+        agent = Agent()
+
+        p_game_state = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]]
+
+        c_game_state = [[0, 0, 0, 1, 0, -1, 0, 0, 0], [1]]
+
+        self.assertEqual(agent.update_previous_game_state_value(p_game_state, c_game_state), [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.55]])
+
+    def test_calculate_new_game_state(self):
+
+        agent = Agent()
+
+        agent.previous_game_state = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0.5]]
+        agent.current_game_state = [[0, 0, 0, 1, 0, -1, 0, 0, 0], [1]]
+
+        self.assertEqual(agent.calculate_new_game_state_value(), [0.55])
 
 if __name__ == '__main__':
     unittest.main()
--- ttt_agent.py
+++ ttt_agent.py
@@ -6,19 +6,9 @@
 # | 6 | 7 | 8 |
 # _____________
 #
-
 from random import randint
+from board_drawer import display_board
 
-from board_drawer import display_board, output_board
-
-winning_state = [[[1, 1, 1, 0, 0, 0, 0, 0, 0], [1]],
-                 [[0, 0, 0, 1, 1, 1, 0, 0, 0], [1]],
-                 [[0, 0, 0, 0, 0, 0, 1, 1, 1], [1]],
-                 [[1, 0, 0, 0, 1, 0, 0, 0, 1], [1]],
-                 [[0, 0, 1, 0, 1, 0, 0, 0, 1], [1]],
-                 [[1, 0, 0, 1, 0, 0, 1, 0, 0], [1]],
-                 [[0, 1, 0, 0, 1, 0, 0, 1, 0], [1]],
-                 [[0, 0, 1, 0, 0, 1, 0, 0, 1], [1]]]
 
 winning_sequences = [[0, 1, 2],
                      [3, 4, 5],
@@ -31,20 +21,20 @@ winning_sequences = [[0, 1, 2],
 
 
 def is_game_won(board, token):
-
     for winning_sequence in winning_sequences:
 
-        # if (board[winning_sequence[0]] and board[winning_sequence[1]] and board[winning_sequence[2]]) is token:
-        #     return True
-
-        if board[winning_sequence[0]] == token and board[winning_sequence[1]] == token and board[winning_sequence[2]] == token:
+        if board[winning_sequence[0]] == token \
+                and board[winning_sequence[1]] == token \
+                and board[winning_sequence[2]] == token:
             return True
 
     return False
 
 
 def is_game_drawn(board):
-    if get_n_of_tokens_on_board(board) < 9:
+    if get_n_of_tokens_on_board(board) == 9 \
+            and not is_game_won(board, 1)\
+            and not is_game_won(board, -1):
         return False
 
     return True
@@ -54,40 +44,41 @@ def create_empty_board():
     return [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
-def subtract_list_from_list(list1, list2):
+def sub_board_from_board(board1, board2):
 
-    if len(list1) is not len(list2):
-        raise Exception
+    if len(board1) is not len(board2):
+        raise Exception('length of board1 is not equal to board2')
 
-    new_list = []
+    sub_list = []
 
-    for i in range(len(list1)):
-        new_element = list2[i] - list1[i]
-        new_list.append(new_element)
+    # each element is subtracted
+    for i in range(len(board1)):
+        new_element = board2[i] - board1[i]
+        sub_list.append(new_element)
 
-    return new_list
+    return sub_list
 
 
 def get_n_of_tokens_on_board(board):
 
-    counter = 0
+    n_tokens = 0
 
     for token in board:
         if token == 1 or token == -1:
-            counter += 1
+            n_tokens += 1
 
-    return counter
+    return n_tokens
 
 
-def get_empty_places_on_board(board):
+def empty_places_on_board(board):
 
-    empty_places_on_board = []
+    empty_places_on_b = []
 
     for i in range(9):
         if board[i] == 0:
-            empty_places_on_board.append(i)
+            empty_places_on_b.append(i)
 
-    return empty_places_on_board
+    return empty_places_on_b
 
 
 def place_token_on_board(board, place, token):
@@ -101,49 +92,44 @@ def place_token_on_board(board, place, token):
     return board
 
 
-def get_next_random_play(board):
+def get_next_random_place(board):
 
-    empty_places_on_board = get_empty_places_on_board(board)
+    empty_places_on_b = empty_places_on_board(board)
 
-    random_number = randint(0, len(empty_places_on_board) - 1)
+    random_number = randint(0, len(empty_places_on_b) - 1)
 
-    return empty_places_on_board[random_number]
+    return empty_places_on_b[random_number]
 
 
 def get_next_random_state(board, token):
-    next_random_place = get_next_random_play(board)
+    next_random_place = get_next_random_place(board)
     board[next_random_place] = token
 
     return create_state_from_board(board, [0.5])
 
 
-def create_new_board(board, place, token):
-    new_board = board
-    new_board[place] = token
-    return new_board
-
-
-def create_state_from_board(board, state_value):
+def create_state_from_board(board, value):
     new_state = [[], []]
 
     new_state[0] = board
-    new_state[1] = state_value
+    new_state[1] = value
     return new_state
 
 
-def get_value_of_game_state(state):
+def state_value(state):
     return state[1][0]
 
 
-def get_place_of_token_to_get_to_next_state(board, next_game_state, token):
+def place_to_get_to_state(board, next_game_state, token):
 
-    subtracted_list = subtract_list_from_list(next_game_state, board)
+    subtracted_list = sub_board_from_board(next_game_state[0], board)
 
     for i in range(len(subtracted_list)):
         if subtracted_list[i] == -token:
             return i
+        else:
+            return 'place to get to next state was not found. board={}, next_game_state={}, token={}'.format(board, next_game_state, token)
 
-    return -1
 
 
 class Agent:
@@ -151,7 +137,10 @@ class Agent:
     def __init__(self):
 
         self.game_states = []
-        self.previous_game_state_value = 0
+        self.previous_game_state = []
+        self.current_game_state = []
+        self.learning_rate = 0.1
+        self.game_state_index = 0
 
     def set_game_states(self, states):
         self.game_states = states
@@ -184,6 +173,24 @@ class Agent:
 
         return next_greedy_game_state
 
+    def update_game_state_in_list(self, updated_game_state):
+        for game_state in self.game_states:
+            if game_state[0] == updated_game_state[0]:
+                game_state[1] = updated_game_state[1]
+
+    def calculate_new_game_state_value(self):
+        new_game_state = self.previous_game_state[1][0] + (self.learning_rate * (self.current_game_state[1][0] - self.previous_game_state[1][0]))
+        return [new_game_state]
+
+    def update_game_state_value(self, game_state, game_state_value):
+        game_state[0][1] = game_state_value
+        return game_state
+
+    def update_game_state_value_of_game_state_in_list(self):
+        new_game_state_value = self.calculate_new_game_state_value()
+        updated_game_state = self.update_game_state_value(self.previous_game_state, new_game_state_value)
+        self.update_game_state_in_list(updated_game_state)
+
     def display_game_states(self):
 
         print('********************* {} *********************'.format('States'))
@@ -201,30 +208,36 @@ class Agent:
 
             next_state = get_next_random_state(old_board, 1)
             self.add_game_state(next_state)
-            next_place = get_place_of_token_to_get_to_next_state(board, next_state[0], 1)
+            next_place = place_to_get_to_state(board, next_state, 1)
 
             board[next_place] = 1
 
             self.display_game_states()
 
-            return board
+            return board, next_state
 
         else:
 
             next_state = self.get_next_greedy_state(get_n_of_tokens_on_board(board), board)
             self.add_game_state(next_state)
-            next_place = get_place_of_token_to_get_to_next_state(board, next_state[0], 1)
+            next_place = place_to_get_to_state(board, next_state, 1)
 
             if next_place is not -1:
                 board[next_place] = 1
 
             self.display_game_states()
 
-            return board
+            return board, next_state
 
     def no_next_state(self, board):
 
         return len(self.game_states) == 0 or len(self.get_next_states(get_n_of_tokens_on_board(board), board)) == 0
+
+    def get_value_of_board_state(self, board):
+
+        for s in self.game_states:
+            if s[0] == board:
+                return s[1]
 
 
 def play():
@@ -235,19 +248,30 @@ def play():
 
     for i in range(200):
 
-        agent.play_turn(board)
+        if len(agent.game_states) is not 0:
+            agent.previous_game_state = agent.current_game_state
+
+        _, next_state = agent.play_turn(board)
+        agent.current_game_state = next_state
+
+        if len(agent.game_states) is not 1:
+            agent.update_game_state_value_of_game_state_in_list()
+
         print(display_board(board))
 
         if is_game_won(board, 1):
+            agent.add_game_state(create_state_from_board(board, [1]))
             print(display_board(board))
             print('-------- X wins --------')
             board = create_empty_board()
+            agent.play_turn(board)
             print(display_board(board))
 
         if is_game_drawn(board):
             print(display_board(board))
             print('-------- DRAW --------')
             board = create_empty_board()
+            agent.play_turn(board)
             print(display_board(board))
 
         place_token_on_board(board, int(input()), -1)
@@ -261,5 +285,4 @@ def play():
 
 if __name__ == '__main__':
    play()
-
 
